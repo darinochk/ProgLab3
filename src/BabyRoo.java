44101ac46997684c83c33f24d878e17ccdbc9cb2
darinchik <darinochk/ProgLab3>
2021-11-30 23:34:52 +0300

    еуче

--- src/BabyRoo.java
+++ src/BabyRoo.java
@@ -1,12 +1,15 @@
 public class BabyRoo extends Animal{
+    public BabyRoo() {
+        this.name = "Крошка Ру";
+    }
+
     public void play(Rabbit rabbit){
         System.out.println("играет c" + rabbit);
     }
-    @Override
-    public String toString(){
-        return "Крошка Ру";
-    }
+
     public void net(){
         System.out.println("Но никого не было. ");
     }
+
+
 }
