e774ed453caad8b5877bdbbdba70afb1ac338679
darinchik <darinochk/ProgLab3>
2022-01-14 14:40:50 +0300

    add constructor to Place

--- src/Kenga.java
+++ src/Kenga.java
@@ -5,7 +5,7 @@ public class Kenga extends Animal {
         super(name, weight, height, birthYear);
     }
 
-    public void say(BabyRoo babyRoo, Rabbit rabbit, Vinny vinny) {
+    public void say(Animal babyRoo, IRabbitActions rabbit, IVinnyActions vinny) {
         System.out.println("Хорошо, - сказала " + name + " про себя, - раз " + babyRoo + ", " + vinny + " и " + rabbit + " решили меня разыграть, я их сама разыграю.");
     }
 
